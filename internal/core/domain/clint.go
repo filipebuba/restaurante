9d52830f95d5fb187e242c1992026e08107ff7a5
filipebuba <filipe.nhada@aluno.faculdadeimpacta.com.br>
2024-08-01 05:53:17 -0300

    fix-id

--- internal/core/domain/clint.go
+++ internal/core/domain/clint.go
@@ -2,10 +2,10 @@ package domain
 
 // Cliente representa um cliente do restaurante
 type Cliente struct {
-	ID        string     `json:"id"`
-	Nome      string     `json:"nome"`
-	Telefone  string     `json:"telefone"`
-	Email     string     `json:"email"`
-	Feedbacks []Feedback `json:"feedbacks"` // Relacionamento: um cliente pode deixar muitos feedbacks
-	Orders    []Order    `json"orders"`     // Relacionamento: um cliente pode fazer muitos pedidos
+	ID        string `json:"id,omitempty"`
+	Nome      string `json:"nome"`
+	Telefone  string `json:"telefone"`
+	Email     string `json:"email"`
+	Feedbacks string `json:"feedbacks"` // Relacionamento: um cliente pode deixar muitos feedbacks
+	Orders    string `json:"orders"`    // Relacionamento: um cliente pode fazer muitos pedidos
 }
