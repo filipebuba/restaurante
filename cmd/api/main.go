6c98862681dc0a8e8d413d940982f899711b0df6
filipebuba <filipe.nhada@aluno.faculdadeimpacta.com.br>
2024-08-14 04:40:39 -0300

    criando pasta cmd

---
+++ cmd/api/main.go
@@ -0,0 +1,64 @@
+/* trunk-ignore-all(gofmt) */
+package main
+
+import (
+	"github.com/filipebuba/restaurante/internal/core/service"
+	handlers "github.com/filipebuba/restaurante/internal/handlers/clients"
+	mysqlrepo "github.com/filipebuba/restaurante/internal/repositories/mysql"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	connect, err := mysqlrepo.GetConnectionDB()
+	if err != nil {
+		return
+	}
+
+	repo := mysqlrepo.NewMySQLRepository(connect)
+	service := service.NewService(repo)
+
+	h := handlers.NewHandler(service)
+
+	// Endpoints para Cliente
+	r.GET("/clients", h.GetClientes)
+	r.GET("/clients/:id", h.GetClienteByID)
+	r.POST("/clients", h.CreateCliente)
+	r.PUT("/clients/:id", h.UpdateCliente)
+	r.DELETE("/clients/:id", h.DeleteCliente)
+
+	// Endpoints para Funcionário
+	//r.GET("/funcionarios", getFuncionarios)
+	//r.POST("/funcionarios", createFuncionario)
+
+	// Endpoints para Menu
+	//r.GET("/menus", getMenus)
+	//r.POST("/menus", createMenu)
+
+	// Endpoints para Food Item
+	//r.GET("/food-items", getFoodItems)
+	//r.POST("/food-items", createFoodItem)
+
+	// Endpoints para Order
+	//r.GET("/orders", getOrders)
+	//r.POST("/orders", createOrder)
+
+	// Endpoints para Invoice
+	//r.GET("/invoices", getInvoices)
+	//r.POST("/invoices", createInvoice)
+
+	// Endpoints para Table
+	//r.GET("/tables", getTables)
+	//r.POST("/tables", createTable)
+
+	// Endpoints para Promoção
+	//r.GET("/promocoes", getPromocoes)
+	//r.POST("/promocoes", createPromocao)
+
+	// Endpoints para Feedback
+	//r.GET("/feedbacks", getFeedbacks)
+	//r.POST("/feedbacks", createFeedback)
+
+	r.Run(":8080") // inicia o servidor na porta 8080
+}
